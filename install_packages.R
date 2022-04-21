225729b04aa4d57b20d37fa79446eea513da3f87
anaelDelorme <anael.delorme@agriculture.gouv.fr>
2022-04-21 17:15:46 +0200

    mise à jour page fullpage avec fond d'image et de videos

---
+++ install_packages.R
@@ -0,0 +1,15 @@
+### A lancer avant le premier build du site web
+
+### R
+install.packages("gganimate")
+install.packages("reticulate")
+install.packages("palmerpenguins")
+install.packages("dygraphs")
+install.packages("tmap")
+install.packages("networkD3")
+
+### python
+reticulate::py_install("matplotlib")
+reticulate::py_install("ipyleaflet")
+reticulate::py_install("plotly")
+
